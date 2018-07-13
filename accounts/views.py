8badfa27929981935302b41be4c87102be621754
zeki.tel <zeki.tel@intra.local>
2018-07-13 13:54:56 +0300

    new version

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/login.html')
+    numbers=[1,2,3,4,5]
+    name='Zeki TEL'
+    args={'myName':name,'numbers':numbers}
+    return render(request,'accounts/home.html',args)
